3ec99bf247a331a703dceb4c6cf685a1f88441a0
jamesleaf <gadejames48@gmail.com>
2022-09-04 02:40:42 +0530

    err

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="services">Services</NavLink>
-            <NavLink to="team">Team</NavLink>
-            <NavLink to="about">About</NavLink>
-            <NavLink to="contact">Contact</NavLink>
+            <NavLink to="/thestackers.in">Home</NavLink>
+            <NavLink to="https://thedevstackers.github.io/thestackers.in/services">Services</NavLink>
+            <NavLink to="https://thedevstackers.github.io/thestackers.in/team">Team</NavLink>
+            <NavLink to="https://thedevstackers.github.io/thestackers.in/about">About</NavLink>
+            <NavLink to="https://thedevstackers.github.io/thestackers.in/contact">Contact</NavLink>
         </nav>
     )
 }
