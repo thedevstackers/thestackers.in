d08eceebee381915359d7e2617afdc59fbd58213
jamesleaf <gadejames48@gmail.com>
2022-09-04 03:10:46 +0530

    pg

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <nav>
             <NavLink to="/thestackers.in">Home</NavLink>
-            <NavLink to="https://thedevstackers.github.io/thestackers.in/services">Services</NavLink>
-            <NavLink to="https://thedevstackers.github.io/thestackers.in/team">Team</NavLink>
-            <NavLink to="https://thedevstackers.github.io/thestackers.in/about">About</NavLink>
-            <NavLink to="https://thedevstackers.github.io/thestackers.in/contact">Contact</NavLink>
+            <NavLink to="services">Services</NavLink>
+            <NavLink to="team">Team</NavLink>
+            <NavLink to="about">About</NavLink>
+            <NavLink to="contact">Contact</NavLink>
         </nav>
     )
 }
